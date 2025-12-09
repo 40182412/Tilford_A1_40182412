29e358951dbbbe855900ea4d0edfc9b6b11d4f72
40182412 <40182412@HY-R104-20.resource.uc>
2025-12-09 13:44:53 +0000

    Assignment 1 class

--- src/main/java/com/mycompany/tilford_a1_40182412/Tilford_A1_40182412.java
+++ src/main/java/com/mycompany/tilford_a1_40182412/Tilford_A1_40182412.java
@@ -10,7 +10,31 @@ package com.mycompany.tilford_a1_40182412;
  */
 public class Tilford_A1_40182412 {
 
+    
+   static int [] list1 = {2, 8, 5, 3,9, 4, 1};
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        
+        for (int i=0; i<list1.length; i++){
+            System.out.println(list1[i]);
+        }
+        
+         for (int i=0; i<list1.length-1; i++){
+            if (list1[i]>list1[i+1]){
+                int temp = list1[i];
+                list1[i] = list1[i+1];
+                list1[i+1] = temp;
+            }
+            System.out.println(list1[i]+",");
+        
+        }
+         
+        
+        }
+        
+        
+        
+        
+        
+        
+   
 }
